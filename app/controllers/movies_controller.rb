8abeef759ed443802b793586bfb6da696fa3b244
Henry Nawrocki Sztanski <henry@sztanski.com>
2024-02-28 20:59:14 -0500

    Add new movie feature and refactor movie form partial

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,40 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
   def index
     @movies = Movie.all
   end
 
   def show
-    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+    if movie.save
+      redirect_to movies_path
+    else
+      render new_movie_path
+    end
   end
 
   def edit
+  end
+
+  def update
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
+  def set_movie
     @movie = Movie.find(params[:id])
   end
 end
