167c786521bf94a892729f62e91c08702b825d32
Henry Nawrocki Sztanski <henry@sztanski.com>
2024-03-04 20:33:04 -0500

    Add director, duration, and image file name fields to movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,7 +37,7 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
   end
 
   def set_movie
