8c9df54a21d3ed2f1bb42cf5f9149af45afa0f5d
Henry Nawrocki Sztanski <henry@sztanski.com>
2024-03-24 18:49:58 -0500

    Add before_action to set_movie in ReviewsController and link to reviews in movie show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
+  def index
+    @reviews = @movie.reviews
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 end
